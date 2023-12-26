b5ec3def228fd1228b2cd9780a01fac3769a067f
Mirzakarieva <144621217+Mirzakarieva@users.noreply.github.com>
2023-12-26 13:57:53 +0500

    day1

---
+++ day1.py
@@ -0,0 +1,25 @@
+urname = input("Enter your name:")
+
+print(len(urname))
+
+
+# Final exrecise
+
+a = input("a: ")
+b = input("b: ")
+
+c=a
+a=b
+b=c
+
+print("a:" , a)
+print("b:" , b)
+
+
+# Final project
+
+city= input("What is the name of you city?\n")
+pet= input("What is the name of a pet?\n")
+
+brand_name = city +" "+ pet
+print("The name of your brand could be " , brand_name)
