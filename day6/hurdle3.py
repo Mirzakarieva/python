f2fcc746b3b548fa7b5d1e09fb8b6a14f9e5d1b0
Mirzakarieva <144621217+Mirzakarieva@users.noreply.github.com>
2023-12-31 23:34:28 +0500

    day6

---
+++ day6/hurdle3.py
@@ -0,0 +1,20 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def fractal():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+    
+# while at_goal() == 0:
+#     if front_is_clear()==1:
+#         move()
+#     if wall_in_front()==1:
+#         fractal()
---
+++ day6/hurdle4.py
@@ -0,0 +1,20 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def fractal():
+#     turn_left()  
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while wall_in_front()==0:
+#         move()
+#     turn_left()
+   
+# while at_goal() == 0:
+#     if wall_in_front()==1:
+#         fractal()
+#     else:
+#         move()
